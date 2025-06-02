f1e211be3785d888cf6b38bb3253520be4ec83f2
Yash Dubey <yash22792@gmail.com>
2025-06-02 23:01:08 +0530

    Lec 10 complete

---
+++ Lec 10 HW/age_cheak.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter your age: ";
+    cin>>age;
+    if(age>=13 && age<=18) cout<<"Teenager";
+    else cout<<"no";
+}
---
+++ Lec 10 HW/even_positive.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the num: ";
+    cin>>n;
+    if(n%2==0 && n>=1) cout<<"yes";
+    else cout<<"no";
+}
